4887acc0155b35ed35dec1f7f4980bfc8e0f87b6
schnoibie <davidjschneider@mizzou.edu>
2016-05-08 11:33:04 -0500

    profile redirect--updating business
    profile page redirects to business page if a business profile is
    selected.
    
    updating business page to function similarly to profile page. still
    finishing

--- Site/business.php
+++ Site/business.php
@@ -3,7 +3,7 @@
 
 <html>
 <head>
-    <title>Link-Book: Buisness</title>
+    <title>Link-Book: Business</title>
     <?php include("header.php");?>
 </head>
 <body>
@@ -14,46 +14,572 @@
     include("navbar.php");
     include("buisnessController.php");
     $buis = getBuisData($_GET[uid]);
-$isEditable = false;
-if($_SESSION[uid] == $_GET[uid]){
-    $isEditable = true;
+
+if($_SESSION[uid] != $_GET[uid]){
+    $_SESSION[isEditable] = false;
 }
-$picPath = $buis[photo];
+    
+
+if($_SESSION[uid] == $_GET[uid]){
+    $_SESSION[isEditable] = true;
+    $_SESSION[bIDnum] = $buis[bIDnum];
+    $_SESSION[name] = $buis[name];
+    $_SESSION[contact_email] = $buis[contact_email];
+    $_SESSION[contact_name] = $buis[contact_name];
+    $_SESSION[biz_size] = $buis[biz_size];
+    $_SESSION[product] = $buis[product];
+    $_SESSION[openings] = $buis[openings];
+    $_SESSION[photo] = $buis[photo];
+
+}
+
+    $picPath = $_SESSION[profile_picture];
+
+
 if($picPath == "empty"){
     $picPath = "../img/no_profile.jpg";
 }
 ?>
+    
+<style>
+
+.updateButton{
+    padding:10px; 
+}
+    
+input{
+    position: static;
+}
+    
+.centered {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  /* bring your own prefixes */
+  transform: translate(-50%, -50%);
+}
+    
+.inputform
+{
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  /* bring your own prefixes */
+  transform: translate(-50%, -50%);
+   width:400px;
+   height:200px;
+   margin-top:10px;
+   margin-left:1px;
+   background-color:#white;
+   opacity: 1;
+   border-radius:3px;
+   box-shadow:0px 0px 10px 0px #424242;
+   padding:10px;
+
+   font-family:helvetica;
+   visibility:hidden;
+   display:none;
+}
+.inputform .close_input
+{
+   position:absolute;
+   top:14px;
+   left:370px;
+   width:15px;
+   height:15px;
+}
+
+.inputform1 .dologin
+{
+   margin-left:5px;
+   margin-top:10px;
+   width:5px;
+   height:40px;
+   border:none;
+   border-radius:3px;
+   color:#E6E6E6;
+   background-color:grey;
+   font-size:20px;
+}
+    
+h4{
+    padding-left:10px;   
+}
+    
+.updateOrg{
+     padding-left:10px;      
+}
+.updateBio{
+     padding-left:10px;      
+}
+    
+.updateCode{
+     padding-left:10px;      
+}
+.aboutText{
+   
+    padding-bottom:5px;
+}
+   
+           
+</style>
+    
+    
+<script>
+$(document).ready(function(){
+
+   $("#show_input1").click(function(){
+    showpopup1();
+   });
+   $("#close_input1").click(function(){
+    hidepopup1();
+   });
+   $("#show_input2").click(function(){
+    showpopup2();
+   });
+   $("#close_input2").click(function(){
+    hidepopup2();
+   });
+   $("#show_input3").click(function(){
+    showpopup3();
+   });
+   $("#close_input3").click(function(){
+    hidepopup3();
+   });
+   $("#show_input4").click(function(){
+    showpopup4();
+   });
+   $("#close_input4").click(function(){
+    hidepopup4();
+   });
+   $("#show_input5").click(function(){
+    showpopup5();
+   });
+   $("#close_input5").click(function(){
+    hidepopup5();
+   });
+
+});
+ 
+    //functions
+function showpopup1()
+{
+   $("#inputform1").fadeIn();
+   $("#inputform1").css({"visibility":"visible","display":"block"});
+}
+function hidepopup1()
+{
+   $("#inputform1").fadeOut();
+   $("#inputform1").css({"visibility":"hidden","display":"none"});
+}  
+function showpopup2()
+{
+   $("#inputform2").fadeIn();
+   $("#inputform2").css({"visibility":"visible","display":"block"});
+}
+function hidepopup2()
+{
+   $("#inputform2").fadeOut();
+   $("#inputform2").css({"visibility":"hidden","display":"none"});
+}
+function showpopup3()
+{
+   $("#inputform3").fadeIn();
+   $("#inputform3").css({"visibility":"visible","display":"block"});
+}
+function hidepopup3()
+{
+   $("#inputform3").fadeOut();
+   $("#inputform3").css({"visibility":"hidden","display":"none"});
+}     
+function showpopup4()
+{
+   $("#inputform4").fadeIn();
+   $("#inputform4").css({"visibility":"visible","display":"block"});
+}
+function hidepopup4()
+{
+   $("#inputform4").fadeOut();
+   $("#inputform4").css({"visibility":"hidden","display":"none"});
+}  
+function showpopup5()
+{
+   $("#inputform5").fadeIn();
+   $("#inputform5").css({"visibility":"visible","display":"block"});
+}
+function hidepopup5()
+{
+   $("#inputform5").fadeOut();
+   $("#inputform5").css({"visibility":"hidden","display":"none"});
+}
+       
+function hideButtons()
+{
+    $("#show_input1").css({"visibility":"hidden","display":"none"});
+}
+function showButtons()
+{
+    $("#show_input1").css({"visibility":"visible","display":"block"});
+}
+     
+if($_SESSION[uid] == $buis[uIDnum]) {
+    showButtons();
+}   
+else{
+    hideButtons();
+}
+    
+</script>
+    
+    
+<script type="text/javascript">
+    function updatePage(){
+        document.getElementById("page").innerHTML = "<?php echo $buis[fName]; echo " "; echo $buis[lName]; ?>";
+    }    
+</script>
+     
+
+    
+    <div class="col-lg-2">
+        
+    <?php if($_SESSION[isEditable]){?>
+        
+
+        
+
+      <?php
+        }       
+
+            if (isset($_POST['updateName'])) {
+                include("../secure/secure.php");
+                $link = mysqli_connect($site, $user, $pass, $db) or die("Connect Error " . mysqli_error($link));
+                
+                $sql = "UPDATE business SET name = ? WHERE uIDnum = ?";
+                //$sql = "UPDATE users SET users(uIDnum, fName, lName, email, username, salt, hashed_pass, organization, bio, profile_picture, coding_languages) VALUES(?,?,?,?,?,?,?,?,?,?,?)";
+                
+                if ($stmt = mysqli_prepare($link, $sql)) {
+                    
+                    $fName = $_SESSION[name] = $_POST['name'];
+                    
+                    $uIDnum = $_SESSION[uid];
+                    $_SESSION[isEditable] = true;
+                    
+                    mysqli_stmt_bind_param($stmt, "ssi", $name, $uIDnum) or die("bind param");
+
+                    
+                    if (mysqli_stmt_execute($stmt)) {   
+
+                    } 
+                    else {
+                        echo "<h4>Failed</h4>";
+                    }
+                } 
+            }
+
+            else if (isset($_POST['updateContEmail'])) {
+                include("../secure/secure.php");
+                $link = mysqli_connect($site, $user, $pass, $db) or die("Connect Error " . mysqli_error($link));
+                
+                $sql = "UPDATE business SET contact_email = ? WHERE uIDnum = ?";
+                
+                if ($stmt = mysqli_prepare($link, $sql)) {
+                    
+                    $email = $_SESSION[contact_email] = $_POST['contact_email']; 
+                    $uIDnum = $_SESSION[uid];
+                    $_SESSION[isEditable] = true;
+                    
+                    echo $fName;
+                    mysqli_stmt_bind_param($stmt, "si", $contact_email, $uIDnum) or die("bind param");
+                    
+                    if (mysqli_stmt_execute($stmt)) {
+
+                    } 
+                    else {
+                        echo "<h4>Failed</h4>";
+                    }
+                } 
+            }
+
+            else if (isset($_POST['updateCode'])) {
+                include("../secure/secure.php");
+                $link = mysqli_connect($site, $user, $pass, $db) or die("Connect Error " . mysqli_error($link));
+                
+                $sql = "UPDATE users SET coding_languages = ? WHERE uIDnum = ?";
+                
+                if ($stmt = mysqli_prepare($link, $sql)) {
+                    
+                    $coding_languages = $_SESSION[coding_languages] = $_POST['coding_languages']; 
+                    $uIDnum = $_SESSION[uid];
+                    $_SESSION[isEditable] = true;
+                    
+                    mysqli_stmt_bind_param($stmt, "si", $coding_languages, $uIDnum) or die("bind param");
+                    
+                    if (mysqli_stmt_execute($stmt)) {
+
+                    } 
+                    else {
+                        echo "<h4>Failed</h4>";
+                    }
+                } 
+            }
+
+            else if (isset($_POST['updateOrg'])) {
+                include("../secure/secure.php");
+                $link = mysqli_connect($site, $user, $pass, $db) or die("Connect Error " . mysqli_error($link));
+                
+                $sql = "UPDATE users SET organization = ? WHERE uIDnum = ?";
+                
+                if ($stmt = mysqli_prepare($link, $sql)) {
+                    
+                    $organization = $_SESSION[organization] = $_POST['organization']; 
+                    $uIDnum = $_SESSION[uid];
+                    $_SESSION[isEditable] = true;
+                    
+                    mysqli_stmt_bind_param($stmt, "si", $organization, $uIDnum) or die("bind param");
+                    
+                    if (mysqli_stmt_execute($stmt)) {
+
+                    } 
+                    else {
+                        echo "<h4>Failed</h4>";
+                    }
+                } 
+            }
+
+            else if (isset($_POST['updateBio'])) {
+                include("../secure/secure.php");
+                $link = mysqli_connect($site, $user, $pass, $db) or die("Connect Error " . mysqli_error($link));
+                
+                $sql = "UPDATE users SET bio = ? WHERE uIDnum = ?";
+                
+                if ($stmt = mysqli_prepare($link, $sql)) {
+                    
+                    $bio = $_SESSION[bio] = $_POST['bio']; 
+                    $uIDnum = $_SESSION[uid];
+                    $_SESSION[isEditable] = true;
+                    
+                    mysqli_stmt_bind_param($stmt, "si", $bio, $uIDnum) or die("bind param");
+                    
+                    if (mysqli_stmt_execute($stmt)) {
+
+                    } 
+                    else {
+                        echo "<h4>Failed</h4>";
+                    }
+                } 
+            }
+
+
+            
+        
+        ?>
+        
+
+    </div>
+    
 <div class="container" style="margin-top: 3em">
+    
     <div class="col-lg-2">
         <div class="row">
             <img src="<?php echo $picPath ?>" style="height: 15em; width: 13em">
+            
+            <div class="col-md-4 col-sm-4 col-xs-3"></div>
+            <div class="col-md-4 col-sm-4 col-xs-6">
+                
+                
+            <form action="<?= $_SERVER['PHP_SELF'] ?>"  method="POST" id="inputform1" class="inputform">
+                
+                <input type="button" type = "image" id = "close_input1" class="close_input" type = "button">
+                
+                <h4> Enter your name: </h4>
+                
+                <div class="updateButton">
+                    <div class="ui input">
+                        <input type="text" name="firstname" placeholder="<?php echo $_SESSION[fName]; ?>" >
+                    </div>
+                </div>
+                <div class="updateButton">
+                    <div class="ui input">
+                        <input type="text" name="lastname" placeholder="<?php echo $_SESSION[lName]; ?>"/>
+                    </div>
+                </div>
+
+                <div class="updateButton">
+                    <input type="submit" name="updateName" value="Update" />
+                </div>
+            </form>
+            
+                
+            <form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST" id="inputform2" class="inputform">
+                
+                <input type="button" type = "image" id = "close_input2" src = "close.png" class="close_input">
+                
+                <h4> Enter your email: </h4>
+                
+                <div class="updateButton">
+                    <div class="ui input">
+                        <input type="email" name="email" placeholder="<?php echo $_SESSION[email]; ?>"/>
+                    </div>
+                </div>
+
+                <div class="updateButton">
+                    <input type="submit" name="updateEmail" required="required"  value="Update"/>
+                </div>
+            </form>       
+                
+                
+            <form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST" id="inputform3" class="inputform">
+                
+                <input type="button" type = "image" id = "close_input3" src = "close.png" class="close_input">
+                
+                <h4> Enter your organizations: </h4>
+                
+                <div class="updateOrg">
+                    <div class="ui input">
+                        <input type="text" name="organization"  placeholder="<?php echo $_SESSION[organization]; ?>"/>
+                    </div>
+                </div>
+
+                <div class="updateButton">
+                    <input type="submit" name="updateOrg"  value="Update"/>
+                </div>
+            </form>       
+                
+                
+ 
+            <form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST" id="inputform4" class="inputform">
+                
+                <input type="button" type = "image" id = "close_input4" src = "close.png" class="close_input">
+                
+                <h4> Enter your coding languages: </h4>
+                
+                <div class="updateCode">
+                    <div class="ui input">
+                        <input type="text" name="coding_languages" placeholder="<?php echo $_SESSION[coding_languages]; ?>"/>
+                    </div>
+                </div>
+
+                <div class="updateButton">
+                    <input type="submit" name="updateCode" value="Update"/>
+                </div>
+            </form>       
+                
+ 
+            <form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST" id="inputform5" class="inputform">
+                
+                <input type="button" type = "image" id = "close_input5" src = "close.png" class="close_input">
+                
+                
+                <div class="updateBio">
+                    <div class="ui input">
+                        <textarea rows="5" cols="40" name="bio"  required="required" > <?php echo $_SESSION[bio]; ?> </textarea>
+                    </div>
+                </div>
+
+                <div class="updateButton">
+                    <input type="submit" name="updateBio" value="Update"/>
+                </div>
+            </form>     
+                
+                
+                
+            </div>        
+                   
         </div>
+
             <?php 
-            printSmallModule($buis[name], $isEditable);
-            printSmallModule($buis[contact_name], $isEditable);
-            printSmallModule($buis[contact_email], $isEditable);
-            printSmallModule($buis[buis_size], $isEditable);
-            printSmallModule($buis[product], $isEditable)
+
+
+            if($_SESSION[isEditable]){
+            printSmallModule($_SESSION[fName]." ".$_SESSION[lName]); ?>
+            <?php if($_SESSION[uid] == $_SESSION[uIDnum]) {?>
+                <input type="button" id="show_input1" value="Edit">
+            <?php } ?>
+            <?php printSmallModule($_SESSION[email]); ?>
+            <?php if($_SESSION[uid] == $_SESSION[uIDnum]) {?>
+                <input type="button" id="show_input2" value="Edit">
+            <?php } ?>
+            <?php printSmallModule($_SESSION[organization]); ?>
+            <?php if($_SESSION[uid] == $_SESSION[uIDnum]) {?>
+                <input type="button" id="show_input3" value="Edit">
+            <?php } ?>
+            <?php printSmallModule($_SESSION[coding_languages]); ?>
+            <?php if($_SESSION[uid] == $_SESSION[uIDnum]) {?>
+                <input type="button" id="show_input4" value="Edit">
+            <?php } 
+            }
+            
             ?>
+        
+            <?php       
+            if($_SESSION[isEditable]==false){
+            printSmallModule($user[fName]." ".$user[lName]); ?>
+            <?php if($_SESSION[uid] == $_SESSION[uIDnum]) {?>
+
+            <?php } ?>
+            <?php printSmallModule($user[email]); ?>
+            <?php if($user[uid] == $user[uIDnum]) {?>
+
+            <?php } ?>
+            <?php printSmallModule($user[organization]); ?>
+            <?php if($_SESSION[uid] == $_SESSION[uIDnum]) {?>
+
+            <?php } ?>
+            <?php printSmallModule($user[coding_languages]); ?>
+            <?php if($_SESSION[uid] == $_SESSION[uIDnum]) {?>
+
+            <?php } 
+
+            }
+            ?>
+
+        
+        
     </div>
     <div class="col-lg-8" style="padding-left: 2em">
+        
+        
+        <h3>About Me:</h3>
+        <div class = "aboutText">
         <?php
-        printBigModule("Job Openings", $buis[openings], $isEditable);
+        if($_SESSION[isEditable]){
+            echo $_SESSION[bio];
+        }
+        if($_SESSION[isEditable]==false){
+            echo $user[bio];            
+        }
+        echo " ";
+            
         //printBigModule("")
         ?>
-    </div>
-   <!--
-    <div class="col-lg-2">
-       <?php // if($isEditable){?>
-        <div class="row"><h5>Update Your Status:</h5></div>
-        <div class="row">
-            <textarea rows="5" placeholder="Status..."
-                      style="height: 5em; resize: none; border-radius: 10px"></textarea>
         </div>
-            <div class="row" style="margin-bottom: 2em"></div>
-    <?php // }
-       // printStatusBlock($buis[uIDnum], 3, 5);?>
+        
+        <?php if($_SESSION[uid] == $_SESSION[uIDnum]) {
+            if($_SESSION[isEditable]){ ?>
+                <input type="button" id="show_input5" value="Edit">
+        <?php
+            }   
+        } ?>
+        
+    </div>
     
-    </div> -->
+        <div class="row"><h5>Update Your Status:</h5>
+            <div class="row">
+             <textarea rows="5" placeholder="Status..."
+                      style="height: 5em; resize: none; border-radius: 10px"></textarea>   
+            </div>
+        <div class="row" style="margin-bottom: 2em"></div>
+            
+         <div class="col-lg-2" class="row">
+        <?php 
+            printStatusBlock($_SESSION[uIDnum], 3, 5);?>    
+        </div>
+            
+            
+        </div>
+
+</div>   
+
+
     
 </html>    
--- SqlScripts/ddl.sql
+++ SqlScripts/ddl.sql
@@ -1,88 +1,157 @@
--- Get rid of old data
-DROP TABLE IF EXISTS linkbook.gen_users;
 DROP TABLE IF EXISTS linkbook.statuses;
-DROP TABLE IF EXISTS linkbook.profiles;
+DROP TABLE IF EXISTS linkbook.profile;
 DROP TABLE IF EXISTS linkbook.buisness;
 DROP TABLE IF EXISTS linkbook.listing;
 DROP TABLE IF EXISTS linkbook.messages;
 DROP TABLE IF EXISTS linkbook.job_history;
 DROP TABLE IF EXISTS linkbook.education;
-DROP TABLE IF EXISTS linkbook.volunteer_work;
+DROP TABLE IF EXISTS linkbook.vol_work;
 DROP TABLE IF EXISTS linkbook.connections;
 
 DROP SCHEMA IF EXISTS linkbook;
 CREATE SCHEMA linkbook;
 
 
-CREATE TABLE linkbook.gen_users
+CREATE TABLE linkbook.users
 (
-uIDnum INT,
+uIDnum INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 fName VARCHAR(30),
 lName VARCHAR(30),
 email VARCHAR(20),
-Orginization VARCHAR(30),
-Bio TEXT,
-job_history TEXT,
-Education VARCHAR(30),
+username VARCHAR(50),
+salt CHAR(40) NOT NULL,
+hashed_pass CHAR(255) NOT NULL,
+orginization VARCHAR(30),
+bio TEXT,
 profile_picture VARCHAR(50),
-volunteer_work TEXT,
-coding_languages VARCHAR(60),
-PRIMARY KEY (uIDnum)
+coding_languages VARCHAR(60)
+-- PRIMARY KEY (uIDnum)
 );
 
-CREATE TABLE  linkbook.statuses
+CREATE TABLE linkbook.connections
 (
-statID INT,
-uIDnum INT,
+conIDnum INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+uIDnum1 INT,
+uIDnum2 INT
+-- PRIMARY KEY (conIDnum),
+-- FOREIGN KEY (uIDnum1) REFERENCES linkbook.users(uIDnum),
+-- FOREIGN KEY (uIDnum2) REFERENCES linkbook.users(uIDnum)
+);
+
+CREATE TABLE linkbook.statuses
+(
+statIDnum INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+ulDnum INT,
 content TEXT,
-PRIMARY KEY (statid),
-FOREIGN KEY (uIDnum) REFERENCES linkbook.gen_users(uIDnum)
+-- PRIMARY KEY (statIDnum),
+FOREIGN KEY (uIDnum) REFERENCES linkbook.users(uIDnum)
 );
 
-CREATE TABLE  linkbook.profiles
+CREATE TABLE linkbook.profile
 (
-proIDnum INT,
-proOwner VARCHAR(30),
+proIDnum INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+uIDnum INT,
 summery TEXT,
 code_links VARCHAR(60),
 project_screenshots VARCHAR(30),
-PRIMARY KEY (proIDnum)
+-- PRIMARY KEY (proIDnum,uIDnum),
+FOREIGN KEY (uIDnum) REFERENCES linkbook.users(uIDnum)
 );
 
-CREATE TABLE  linkbook.buisness
+CREATE TABLE linkbook.business
 (
 uIDnum INT,
-bIDnum INT,
+bIDnum INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 name VARCHAR(30),
 contact_email VARCHAR(30),
 contact_name VARCHAR(30),
-size INT,
+biz_size INT,
 product VARCHAR(30),
 openings INT,
 photo VARCHAR(30),
-PRIMARY KEY (uIDnum, bIDnum),
-FOREIGN KEY (uIDnum) REFERENCES linkbook.gen_users(uIDnum)
+-- PRIMARY KEY (uIDnum, bIDnum),
+FOREIGN KEY (uIDnum) REFERENCES linkbook.users(uIDnum)
 );
 
-CREATE TABLE  linkbook.listing
+CREATE INDEX ix_bIDnum ON linkbook.business(bIDnum);
+
+CREATE TABLE linkbook.listing
 (
+listIDnum INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 bIDnum INT,
 job_title VARCHAR(30),
 job_description VARCHAR(600),
 qualifications VARCHAR(600),
 starting_pay VARCHAR(30),
-PRIMARY KEY (bIDnum)
+-- PRIMARY KEY (listIDnum),
+FOREIGN KEY (bIDnum) REFERENCES linkbook.buisness(bIDnum)
 );
 
-CREATE TABLE  linkbook.messages
+CREATE TABLE linkbook.messages
 (
-msgIDnum INT,
+msgIDnum INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 send_timestamp TIMESTAMP,
 sender_uIDnum INT,
 receiver_uIDnum INT,
 receiver_timestamp TIMESTAMP,
-contents TEXT,
-PRIMARY KEY (msgIDnum),
-FOREIGN KEY (sender_uIDnum) REFERENCES linkbook.gen_users(uIDnum),
-FOREIGN KEY (receiver_uIDnum) REFERENCES linkbook.gen_users(uIDnum),
+contents TEXT
+-- PRIMARY KEY (msgIDnum)
+/*
+FOREIGN KEY (sender_uIDnum) REFERENCES linkbook.users(uIDnum),
+FOREIGN KEY (receiver_uIDnum) REFERENCES linkbook.users(uIDnum)
+*/
 );
+
+
+CREATE TABLE linkbook.education
+(
+uIDnum INT,
+eduIDnum INT NOT NULL AUTO_INCREMENT,
+institution TEXT,
+degree TEXT,
+PRIMARY KEY (uIDnum, eduIDnum),
+FOREIGN KEY (uIDnum) REFERENCES linkbook.users(uIDnum)
+);
+
+CREATE TABLE linkbook.job_history
+(
+uIDnum INT,
+jobIDnum INT NOT NULL AUTO_INCREMENT,
+company VARCHAR(100),
+job_title VARCHAR(100),
+PRIMARY KEY (uIDnum, jobIDnum),
+FOREIGN KEY (uIDnum) REFERENCES linkbook.users(uIDnum)
+);
+
+CREATE TABLE linkbook.vol_work
+(
+uIDnum INT,
+volIDnum INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+orginization VARCHAR(100),
+position VARCHAR(100),
+-- PRIMARY KEY (uIDnum, volIDnum),
+FOREIGN KEY (uIDnum) REFERENCES linkbook.users(uIDnum)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
